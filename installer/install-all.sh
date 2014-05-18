a8e79024e4616712675f5614d756677b162de345
Arnaud Schoonjans <arnaud.schoonjans@student.kuleuven.be>
2014-05-18 09:39:11 +0000

    installer update

--- installer/install-all.sh
+++ installer/install-all.sh
@@ -11,9 +11,3 @@ mkdir imp-modules
 /root/YCSB/installer/create_riak_project.sh
 # Install R
 yum -y install R
-# Disable firewall
-setenforce 0
-sed -i "s/SELINUX=enforcing/SELINUX=permissive/g" /etc/sysconfig/selinux
-systemctl disable firewalld
-# create ssh-keys
-ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa
