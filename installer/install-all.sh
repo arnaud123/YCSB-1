1bbcd6c5022dd1fd4e833399870e0eb27ff59f17
Arnaud Schoonjans <arnaud.schoonjans@student.kuleuven.be>
2014-06-23 16:58:04 +0200

    Install maven and build YCSB in installer

--- installer/install-all.sh
+++ installer/install-all.sh
@@ -9,5 +9,7 @@ mkdir imp-projects
 /root/YCSB/installer/create_couchdb_project.sh
 /root/YCSB/installer/create_mysqlCluster_project.sh
 /root/YCSB/installer/create_riak_project.sh
-# Install R
-yum -y install R
+# Install R and maven
+yum -y install R maven
+# Build YCSB
+mvn clean package
