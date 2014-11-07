70a488b935c4428feae1df07a0c600d679559916
Arnaud Schoonjans <arnaud.schoonjans@cs.kuleuven.be>
2014-11-07 17:05:47 +0100

    MongoDb specific parameter is cluster class

--- front_end/cluster/MongoDbCluster.py
+++ front_end/cluster/MongoDbCluster.py
@@ -3,13 +3,42 @@ from cluster.Cluster import Cluster
 from delete_data.deleteAllMongoDbData import deleteAllDataInMongoDb
 
 class MongoDbCluster(Cluster):
-    
+
+    _MAX_AMOUNT_OF_CONNECTION = 100
+
     def __init__(self, normalBinding, consistencyBinding, nodesInCluster, accessNodes):
         super().__init__(normalBinding, consistencyBinding, nodesInCluster)
         self.__databaseName = "ycsb"
         self.__collectionName = "usertable"
         self._accessNodes = accessNodes
-        
+
+    def getLoadCommand(self, pathToWorkloadFile, extraParameters = []):
+        extraParameters = self._addMongoDbSpecificProperties(extraParameters);
+        return super(MongoDbCluster, self).getLoadCommand(pathToWorkloadFile, extraParameters);
+
+    def getRunCommand(self, pathToWorkloadFile, runtimeBenchmarkInMinutes, amountOfThreads, extraParameters = []):
+        extraParameters = self._addMongoDbSpecificProperties(extraParameters);
+        return super(MongoDbCluster, self).getRunCommand(pathToWorkloadFile, runtimeBenchmarkInMinutes, amountOfThreads, extraParameters);
+
+    def getConsistencyRunCommand(self, pathToWorkloadFile, pathConsistencyResult, runtimeBenchmarkInSeconds, amountOfThreads, extraParameters = []):
+        extraParameters = self._addMongoDbSpecificProperties(extraParameters);
+        return super(MongoDbCluster, self).getConsistencyRunCommand(pathToWorkloadFile, pathConsistencyResult, runtimeBenchmarkInSeconds, amountOfThreads, extraParameters);\
+
+    def _addMongoDbSpecificProperties(self, paramList):
+        paramList = self._addUrlPropertyToDbBinding(paramList)
+        paramList = self._addAmountOfConnectionsToDbBinding(paramList)
+        return paramList
+
+    def _addUrlPropertyToDbBinding(self, paramList):
+        paramList.append('-P')
+        paramList.append("mongodb://" + self._accessNodes[0] + ":27017")
+        return paramList
+
+    def _addAmountOfConnectionsToDbBinding(self, paramList):
+        paramList.append('-P')
+        paramList.append(str(MongoDbCluster._MAX_AMOUNT_OF_CONNECTION))
+        return paramList
+
     def deleteDataInCluster(self):
         accessNode = self._accessNodes[0]
         deleteAllDataInMongoDb(accessNode, self.getNodesInCluster(), self.__databaseName, self.__collectionName)
