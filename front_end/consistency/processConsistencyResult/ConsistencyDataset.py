8731c49ddf8c8516f5158e52319d5c679b69763e
Arnaud Schoonjans <arnaud.schoonjans@cs.kuleuven.be>
2014-11-26 10:34:03 +0100

    Bugfix: Remove warm-up data

--- front_end/consistency/processConsistencyResult/ConsistencyDataset.py
+++ front_end/consistency/processConsistencyResult/ConsistencyDataset.py
@@ -32,14 +32,7 @@ class ConsistencyDataset(object):
 
     def _removeWriteReadPairsInWarmupPhase(self, writeReadPairs, warmUpInMicros):
         smallestTimestamp = self._getSmallestTimestamp(writeReadPairs)
-        indicesToRemove = []
-        for i in range(len(writeReadPairs)):
-            pair = writeReadPairs[i]
-            if pair.getTimestamp() < smallestTimestamp + warmUpInMicros:
-                indicesToRemove.append(i)
-        for i in indicesToRemove:
-            writeReadPairs.pop(i)
-        return writeReadPairs
+        return [pair for pair in writeReadPairs if pair.getTimestamp() >= smallestTimestamp + warmUpInMicros]
 
     def _getSmallestTimestamp(self, writeReadPairs):
         smallestTimestamp = writeReadPairs[0].getTimestamp()
