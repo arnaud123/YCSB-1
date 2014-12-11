a8da4e8cf89424e44cea149e99414ada9ce5bc0a
Arnaud Schoonjans <arnaud.schoonjans@cs.kuleuven.be>
2014-12-11 11:20:03 +0100

    Add field length property to run command

--- front_end/util/ycsbCommands/Commands.py
+++ front_end/util/ycsbCommands/Commands.py
@@ -4,7 +4,7 @@ TIMESERIES_GRANULARITY = 2000;
 def getLoadCommand(binding, pathToWorkloadFile, extraParameters = []):
     runCommand = [PATH_YCSB_EXECUTABLE, 'load', binding]; 
     runCommand.extend(['-P', pathToWorkloadFile]);
-    runCommand.extend(extraParameters);
+    runCommand.extend(extraParameters);z
     runCommand.extend(['-threads', str(200)]);
     runCommand.append('-s');
     return runCommand;
@@ -15,6 +15,7 @@ def getRunCommand(binding, pathToWorkloadFile, runtimeBenchmarkInMinutes, amount
     runCommand.extend(['-p', 'measurementtype=timeseries']); 
     runCommand.extend(['-p', 'timeseries.granularity=' + str(TIMESERIES_GRANULARITY)]);
     runCommand.extend(['-p', 'maxexecutiontime=' + str(runtimeBenchmarkInMinutes*60)]);
+    runCommand.extend(['-p', 'fieldlength=110']);
     runCommand.extend(['-threads', amountOfThreads]);
     runCommand.extend(extraParameters);
     runCommand.append('-s');
