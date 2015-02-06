d643f825091fe00db0c48866f9c4085e2ef37c8f
Arnaud Schoonjans <arnaud.schoonjans@cs.kuleuven.be>
2015-02-06 08:04:16 +0000

    setting fieldcount and field length

--- front_end/util/ycsbCommands/Commands.py
+++ front_end/util/ycsbCommands/Commands.py
@@ -1,10 +1,15 @@
 PATH_YCSB_EXECUTABLE = "bin/ycsb";
 TIMESERIES_GRANULARITY = 2000;
 
+FIELD_LENGTH = "100";
+FIELD_COUNT = "100";
+
 def getLoadCommand(binding, pathToWorkloadFile, extraParameters = []):
     runCommand = [PATH_YCSB_EXECUTABLE, 'load', binding]; 
     runCommand.extend(['-P', pathToWorkloadFile]);
     runCommand.extend(extraParameters);
+    runCommand.extend(['-p', 'fieldlength=' + FIELD_LENGTH]);
+    runCommand.extend(['-p', 'fieldcount=' + FIELD_COUNT]);   
     runCommand.extend(['-threads', str(50)]);
     runCommand.append('-s');
     return runCommand;
@@ -15,8 +20,8 @@ def getRunCommand(binding, pathToWorkloadFile, runtimeBenchmarkInMinutes, amount
     runCommand.extend(['-p', 'measurementtype=timeseries']); 
     runCommand.extend(['-p', 'timeseries.granularity=' + str(TIMESERIES_GRANULARITY)]);
     runCommand.extend(['-p', 'maxexecutiontime=' + str(runtimeBenchmarkInMinutes*60)]);
-    runCommand.extend(['-p', 'fieldlength=100']);
-    runCommand.extend(['-p', 'fieldcount=100']);   
+    runCommand.extend(['-p', 'fieldlength=' + FIELD_LENGTH]);
+    runCommand.extend(['-p', 'fieldcount=' + FIELD_COUNT]);   
     runCommand.extend(['-threads', amountOfThreads]);
     runCommand.extend(extraParameters);
     runCommand.append('-s');
